875484daa8e7d0ebb7a17d802afa2cf7452b56e4
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-07-26 22:58:00 +0200

    installation de : react-icons et leaflet.fullscreen dans la partie Frontend

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { forgotPassword, resetPassword, loginUser } = require("../controllers/userController");
+const { forgotPassword, resetPassword } = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
 // Réinitialisaion du mot de passe
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
 //Authentification
-router.post("/login", loginUser);
+router.post("/login", authController.login);
 
 module.exports = router;
